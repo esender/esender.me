d589e37e7fc58bb2c7fe1e27efbb0dcec1fe22bd
Marat Abdulin <esend.work@gmail.com>
2023-05-20 22:13:07 +0200

    About page

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define your collection(s)
+const companies = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    start_end_months: z.string(),
+    position: z.string(),
+    stack: z.array(z.string()),
+    description: z.array(z.string()).optional(),
+    points: z.array(z.string()).optional(),
+    order: z.number(),
+  }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  "work-experience": companies,
+};
