d57bbbc3c1667612ae9acab48a5577b27fb90a67
Marat Abdulin <esend.work@gmail.com>
2023-07-09 21:00:45 +0200

    Bump packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      config: { applyBaseStyles: false },
+      applyBaseStyles: false,
     }),
   ],
 });
